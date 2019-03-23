186908fa8e58a62ae5af61ac33a5236ae5b719f3
Islam Mesabah <islamkaloop@gmail.com>
2019-03-22 23:28:08 -0700

    get all admins

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -1,87 +1,18 @@
 const express = require('express');
-const uuid =require('uuid');
 const router = express.Router();
+
 const Admin =require('../../models/Admin');
-const Joi = require('joi');
 
-const admins =[
-    new Admin('admin1','admin1@gmail.com','Ad1233','123456'),
-    new Admin('admin2','admin2@gmail.com','Ad1234','133456'),
-    new Admin('admin3','admin3@gmail.com','Ad1235','143456')
-];
 // Get all admins
-router.get('/', (req,res) => res.json({admins : admins}));
+router.get('/', async (req,res) => {
+    try{
+        const admins = await Admin.find();
+        if(admins.length==0) res.status(404).send({error:'Admin was not found'});
+        res.json({admins : admins});
+    }catch(error) {
+        res.json({error:error.message});
+    } 
+})
 
-// Get a certain admin
-router.get('/:id', (req, res) => {
-    const adminId = req.params.id;
-    const admin =admins.find(admin => admin.id === adminId);  
-    res.json(admin);
-});
-
-
-// Create a admin
-router.post('/', (req, res) => {
-    const name = req.body.name;
-
-    const email = req.body.email;
-    const password = req.body.password;
-    const phone = req.body.phone;
-
-    const schema = {
-
-		name: Joi.string().required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().min(8).alphanum().required(),
-        phone: Joi.number().required()     
-	}
-
-    const result = Joi.validate(req.body, schema);
-
-    if (result.error) return res.status(400).send({ error: result.error.details[0].message });
-
-
-    const admin =  new Admin(name,email,password,phone);
-    admins.push(admin);
-    res.json({admins : admins});
-});
-
-// Update a admin info
-router.put('/:id', (req, res) => {
-    const adminId = req.params.id; 
-    const name = req.body.name;
-
-    const email = req.body.email;
-    const password = req.body.password;
-    const phone = req.body.phone;
-    const admin = admins.find(admin => admin.id === adminId);
-
-    const schema = {
-        name: Joi.string(),
-        email: Joi.string().email(),
-        password: Joi.string().min(8).alphanum(),
-        phone: Joi.number()
-	}
-
-    const result = Joi.validate(req.body, schema);
-    if (result.error) return res.status(400).send({ error: result.error.details[0].message });
-
-    
-    if(name)admin.name=name;
-    if(email)admin.email=email;
-    if(password)admin.password=password;
-    if(phone)admin.phone=phone;
-    res.json({admins : admins});
-});
-
-
-// Delete a admin
-router.delete('/:id', (req, res) => {
-    const adminId = req.params.id;
-    const admin = admins.find(admin => admin.id === adminId);
-    const index = admins.indexOf(admin);
-    admins.splice(index,1);
-    res.json({admins : admins});
-});
 
 module.exports = router
