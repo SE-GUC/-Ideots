7ea7b30ddf3f40b0719eed5bf9f9d5cd84a63d3a
Islam Mesabah <islamkaloop@gmail.com>
2019-03-22 23:33:20 -0700

    Insert Admin

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt =require('bcryptjs');
 const router = express.Router();
 
 const Admin =require('../../models/Admin');
@@ -22,6 +23,26 @@ router.get('/:id', async (req, res) => {
         if(!admin) res.status(404).send({error:'Admin was not found'});
         res.json(admin);
     }catch(error) {
+        res.json({error:error.message});
+   } 
+});
+
+// Create a admin
+router.post('/', async (req, res) => {
+    try{
+        const isValidated = validator.createValidation(req.body);
+        if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
+        const salt =bcrypt.genSaltSync(10);
+        const passAfterHashing =bcrypt.hashSync(req.body.password,salt);
+        const newAdmin =await Admin.create({
+            name:req.body.name,
+            email:req.body.email,
+            password: passAfterHashing,
+            phone:req.body.phone
+        });
+        res.json({msg:'Admin was created successfully', data: newAdmin});
+   }
+   catch(error) {
         res.json({error:error.message});
    } 
 });
