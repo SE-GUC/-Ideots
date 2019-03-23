823ee468c8aed276847107ff27ff288fba5a6165
Islam Mesabah <islamkaloop@gmail.com>
2019-03-22 23:29:52 -0700

    Get a certain admin

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -14,5 +14,17 @@ router.get('/', async (req,res) => {
     } 
 })
 
+// Get a certain admin
+router.get('/:id', async (req, res) => {
+    try{
+        const adminId = req.params.id;
+        const admin =await Admin.findOne({_id:adminId});
+        if(!admin) res.status(404).send({error:'Admin was not found'});
+        res.json(admin);
+    }catch(error) {
+        res.json({error:error.message});
+   } 
+});
+
 
 module.exports = router
