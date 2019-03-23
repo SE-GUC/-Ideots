6b4b72b8332789be9f767dd98ef64d97dfcecbab
Mohammed Hossam <31958366+MohammedHossam@users.noreply.github.com>
2019-03-23 23:58:58 +0200

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-// module.exports = {
-//     mongoURI: 'mongodb+srv://mohamed_ashry:LX96TG9X0PSkWoLi@ideots-viyif.mongodb.net/test?retryWrites=true',
-// }
-module.exports = {
-    mongoURI: 'mongodb+srv://bo-g:A1B2C3d@cluster0-golck.mongodb.net/test?retryWrites=true',
-}
