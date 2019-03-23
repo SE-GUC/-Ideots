6a37610303db5a753f1a581d9884e52dcdce523c
AhmedSoror <ahmedsoror438@gmail.com>
2019-03-23 21:42:14 +0200

    Notification migrated to mongo

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,6 +1,3 @@
-// module.exports = {
-//     mongoURI: 'mongodb+srv://mohamed_ashry:LX96TG9X0PSkWoLi@ideots-viyif.mongodb.net/test?retryWrites=true',
-// }
 module.exports = {
-    mongoURI: 'mongodb+srv://abdo_elbeltagy:5ANUGTrn1K9swEUq@ideots-viyif.mongodb.net/collection_LirtenDB?retryWrites=true',
+    mongoURI:'mongodb+srv://abdo_elbeltagy:5ANUGTrn1K9swEUq@ideots-viyif.mongodb.net/Lirten_DB?retryWrites=true'
 }
