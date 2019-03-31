40802c24a50c81fb5b1c5ef2a2cc5416fb38bbf5
MohammedHossam <mimo.mhf@gmail.com>
2019-04-01 01:14:10 +0200

    Update

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://bo-g:A1B2C3d@cluster0-golck.mongodb.net/test?retryWrites=true'
+    mongoURI:'mongodb+srv://abdo_elbeltagy:5ANUGTrn1K9swEUq@ideots-viyif.mongodb.net/Lirten_DB?retryWrites=true'
 }
 
 
