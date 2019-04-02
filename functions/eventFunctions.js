97d36de9b4d73807428d0f0cbbef18980df0168a
mohamed-ashry7 <mohamed.ashrymahmoud@student.guc.edu.eg>
2019-04-02 04:30:50 +0200

    making the tests consistent with the real database

--- functions/eventFunctions.js
+++ functions/eventFunctions.js
@@ -18,6 +18,7 @@ const functions = {
         return theEvent 
     } , 
     getAnEventByType : async (type)=>{
+        console.log(type)
         theEvent = await axios.get(`http://localhost:3000/api/events/search/${type}`)
         return theEvent 
     },
--- test/event.test.js
+++ test/event.test.js
@@ -7,61 +7,9 @@ const fn = require("../functions/eventFunctions");
 //    // console.log(allEvents)
 // });
 
-test("Searching for an Event by ID ", async () => {
-  // expect.assertions(1)
-  const theEvent = await fn.getAnEventByID("5c9534d9245cba7ddab501dc"); // this is and ID from the test DB
-  const id = theEvent.data.data._id;
-  expect(id).toEqual("5c9534d9245cba7ddab501dc");
-});
 
-//gimme an err 
-test("Searching for a non existent Event by ID ", async  () => {
-  
-  const eve = await  fn.getAnEventByID("5c9534d9245cba7ddab501dD")
-  console.log(eve)
-
-   expect(Object.keys(eve.data.data)).toContainEqual({error: 'The Event you are tryinig to show does not exist '})
-
-});
-
-test("Getting an Event By Location Of City , Area and Street ", async () => {
-  const theEvents = await fn.getAnEventUsingLocation(
-    "cairo",
-    "rehab",
-    "youssef el sebaie"
-  ); // also this an instance in the DB
-  const len = theEvents.data.data.length;
-  expect(len).toBeGreaterThanOrEqual(1);
-});
-
-test("Getting non existent Event By Location Of City , Area and Street ", async () => {
-  const theEvents = await fn.getAnEventUsingLocation(
-    "hurghada",
-    "al dahar",
-    "al moror al kadeem "
-  ); // also this an instance in the DB
-  const len = theEvents.data.data.length;
-  expect(len).toEqual(0);
-});
-
-
-test ("searching for an event by type " , async() => { 
-    const theEvents = await fn.getAnEventByType(";new;old")
-    const len = theEvents.data.data.length 
-    expect(len).toBeGreaterThanOrEqual(1);
-
-});
-// test ("searching for non existent event by type " , async () => { 
-//     const eve = await fn.getAnEventByType("Haribo")
-//     console.log(eve.status)
-//     // const msg = eve._errorDetails._status ; 
-//     expect(msg).toEqual("404")
-
-    
-
-// })
-
-test ("posting a new event " , async()=>{
+describe("testing the event entity " , ()=>{
+  test ("posting a new event " , async()=>{
     const params = {
         location: 
             {  
@@ -71,7 +19,7 @@ test ("posting a new event " , async()=>{
             }
         , 
         description: "event in hurghada kabf kjb kjabsfk bakdfb akbf kjabfkj habjfbh ajsfhb jabshf jhbasfj akjafsb kjb ",
-        type: "for fun ",
+        type: "forFun",
         registrationPrice: 150,
         numberOfSpaces: 199,
         speakers: ["ashry" , "akkad"],
@@ -84,25 +32,85 @@ test ("posting a new event " , async()=>{
     }
     const newEvent = await fn.postNewEvent(params)
     expect(newEvent.data.data.description).toEqual("event in hurghada kabf kjb kjabsfk bakdfb akbf kjabfkj habjfbh ajsfhb jabshf jhbasfj akjafsb kjb ")
-});
-
-
-test("editing an event " , async() => {
-
-    const params = {
-        description : "this a meaningless description used for testing and just help me exceed the thirt charachters i wish to exceedddddddddddddddddddddddddddddddd "
-    }
-    const updatedEvent = await fn.updatingAnEvent("5c9534d9245cba7ddab501dc" , params)
-    console.log(updatedEvent.config.data)
-    expect(updatedEvent.config.data).toEqual(`{"description":"this a meaningless description used for testing and just help me exceed the thirt charachters i wish to exceedddddddddddddddddddddddddddddddd "}`) ; 
-
-});
-
-test ("deleting an Event " , async()=> { 
+  });
+  
+  test("Searching for an Event by ID ", async () => {
+    // expect.assertions(1)
     const allBefore = await fn.getAllEvents()
     const id = allBefore.data.data.length-1
-    const deletedEvent = await fn.deleteAnEvent(allBefore.data.data[id]["_id"])
-    const allAfter = await fn.getAllEvents()
-    expect(allAfter.data.data.length).toBe(allBefore.data.data.length - 1 ) 
-
+    const EventID = allBefore.data.data[id]["_id"] 
+    const theEvent = await fn.getAnEventByID(EventID); // this is and ID from the test DB
+    const idreceived = theEvent.data.data._id;
+    expect(idreceived).toEqual(EventID);
+  });
+  
+  //gimme an err 
+  // test("Searching for a non existent Event by ID ", async  () => {
+    
+  //   const eve = await  fn.getAnEventByID("5c9534d9245cba7ddab501dD")
+  //   console.log(eve)
+  
+  //    expect(Object.keys(eve.data.data)).toContainEqual({error: 'The Event you are tryinig to show does not exist '})
+  
+  // });
+  
+  test("Getting an Event By Location Of City , Area and Street ", async () => {
+    const theEvents = await fn.getAnEventUsingLocation(
+      "hurghada",
+      "al kawathar" , 
+      "al mamsha"  
+                
+    ); // also this an instance in the DB
+    const len = theEvents.data.data.length;
+    expect(len).toBeGreaterThanOrEqual(1);
+  });
+  
+  test("Getting non existent Event By Location Of City , Area and Street ", async () => {
+    const theEvents = await fn.getAnEventUsingLocation(
+      "jijijij",
+      "al dahar",
+      "al moror al kadeem "
+    ); // also this an instance in the DB
+    const len = theEvents.data.data.length;
+    expect(len).toEqual(0);
+  });
+  
+  test ("searching for an event by type " , async() => { 
+      const theEvents = await fn.getAnEventByType("forFun")
+      const len = theEvents.data.data.length 
+      expect(len).toBeGreaterThanOrEqual(1);
+  
+  });
+  // test ("searching for non existent event by type " , async () => { 
+  //     const eve = await fn.getAnEventByType("Haribo")
+  //     console.log(eve.status)
+  //     // const msg = eve._errorDetails._status ; 
+  //     expect(msg).toEqual("404")
+  
+      
+  
+  // })
+  
+  
+  
+  
+  test("editing an event " , async() => {
+  
+      const params = {
+          description : "this a meaningless description used for testing and just help me exceed the thirt charachters i wish to exceedddddddddddddddddddddddddddddddd "
+      }
+      const updatedEvent = await fn.updatingAnEvent("5c9534d9245cba7ddab501dc" , params)
+      console.log(updatedEvent.config.data)
+      expect(updatedEvent.config.data).toEqual(`{"description":"this a meaningless description used for testing and just help me exceed the thirt charachters i wish to exceedddddddddddddddddddddddddddddddd "}`) ; 
+  
+  });
+  
+  test ("deleting an Event " , async()=> { 
+      const allBefore = await fn.getAllEvents()
+      const id = allBefore.data.data.length-1
+      const deletedEvent = await fn.deleteAnEvent(allBefore.data.data[id]["_id"])
+      const allAfter = await fn.getAllEvents()
+      expect(allAfter.data.data.length).toBe(allBefore.data.data.length - 1 ) 
+  
+  })
 })
