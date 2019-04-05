d15f525d9a3b4d01a8ec032e68b1b92c911c061b
Islam Kaloop <37816680+islamkaloop@users.noreply.github.com>
2019-04-05 21:52:37 +0200

    add controller to user

---
+++ controllers/users.js
@@ -0,0 +1,99 @@
+const User =require('../../models/User');
+const validator =require('../../validations/userValidations');
+
+const users = {
+    postUser :async (req, res) => {
+        try{
+            const email = req.body.email;
+            const user = await User.findOne({email});
+            if(user) return res.status(400).json({error:"There are User with this email"});
+            const salt =bcrypt.genSaltSync(10);
+            const passAfterHashing =bcrypt.hashSync(req.body.password,salt);
+            const type = req.body.type;
+            if(type=='member'){
+                const isValidated = validator.createValidationMember(req.body);
+                if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
+                const newUser =await User.create({
+                    type:"member",
+                    name:req.body.name,
+                    email:req.body.email,
+                    password: passAfterHashing,
+                    phoneNumber:req.body.phoneNumber,
+                    birthDate:req.body.birthDate,
+                    location:req.body.location,
+                    otherContacts:req.body.otherContacts,
+                    field:req.body.field,
+                    skills:req.body.skills,
+                    interests:req.body.interests,
+                    tasks:req.body.tasks,
+                    attendedEvents:req.body.attendedEvents,
+                    experience:req.body.experience,
+                    certificates:req.body.certificates
+                });
+                res.json({msg:'User was created successfully', data: newUser});
+            }else if(type=='partner'){
+                const isValidated = validator.createValidationPartner(req.body);
+                if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
+                const newUser =await User.create({
+                    type:"partner",
+                    name:req.body.name,
+                    email:req.body.email,
+                    password: passAfterHashing,
+                    basicBussinesInformation:req.body.basicBussinesInformation,
+                    boardMembers:req.body.boardMembers,
+                    fieldOfWork:req.body.fieldOfWork,
+                    partners:req.body.partners,
+                    eventOrganized:req.body.eventOrganized,
+                    formFeedBack:req.body.formFeedBack,
+                    pastProjects:req.body.pastProjects,
+                    contactInfo:req.body.contactInfo
+                });
+                res.json({msg:'User was created successfully', data: newUser});
+            }else{
+                const isValidated = validator.createValidationConsaltancyAgency(req.body);
+                if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
+                const newUser =await User.create({
+                    type:"consultancy_agency",
+                    name:req.body.name,
+                    email:req.body.email,
+                    password: passAfterHashing,
+                    description:req.body.description,
+                    specialization:req.body.specialization,
+                    website:req.body.website,
+                    fax:req.body.fax,
+                    address:req.body.address,
+                    reports:req.body.reports,
+                    boardMembers:req.body.boardMembers,
+                    partners:req.body.partners,
+                    events:req.body.events
+                });
+                res.json({msg:'User was created successfully', data: newUser});
+            }
+       }
+       catch(error) {
+            res.json({error:error.message});
+       } 
+    },
+    putUser :async (req, res) => {     
+        const id = req.params.id;
+        const user = await User.findOne({_id:id});
+        if(!user) return res.status(404).send({error: 'User does not exist'});   
+       
+        if(email){
+            const user = await User.findOne({email});
+            if(user & user._id != id) return res.status(400).json({error:"There are User with this emial"});
+        }
+        let result =null ;
+        if (user.type=='member')
+        {result = validator.updateValidationMember(req.body) }
+        if (user.type=='partner')
+        {  result = validator.updateValidationPartner(req.body)} 
+         else
+        { result = validator.updateValidationConsaltancyAgency(req.body) }
+        
+        if (result.error) return res.status(400).send({ error: result.error.details[0].message })
+        const updatedUser = await User.updateOne({ "_id" : id },req.body)
+        res.json({data:updatedUser})
+    }
+};
+module.exports = users;
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const bcrypt =require('bcryptjs');
 const router = express.Router();
 
 const User =require('../../models/User');
-const validator =require('../../validations/userValidations');
+const users =require("../../controllers/users")
 
 // Get all users
 router.get('/', async (req,res) => {
@@ -41,78 +41,8 @@ router.get('/:id', async (req, res) => {
 });
 
 // Create a user
-router.post('/', async (req, res) => {
-    try{
-        const email = req.body.email;
-        const user = await User.findOne({email});
-        if(user) return res.status(400).json({error:"There are User with this email"});
-        const salt =bcrypt.genSaltSync(10);
-        const passAfterHashing =bcrypt.hashSync(req.body.password,salt);
-        const type = req.body.type;
-        if(type=='member'){
-            const isValidated = validator.createValidationMember(req.body);
-            if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
-            const newUser =await User.create({
-                type:"member",
-                name:req.body.name,
-                email:req.body.email,
-                password: passAfterHashing,
-                phoneNumber:req.body.phoneNumber,
-                birthDate:req.body.birthDate,
-                location:req.body.location,
-                otherContacts:req.body.otherContacts,
-                field:req.body.field,
-                skills:req.body.skills,
-                interests:req.body.interests,
-                tasks:req.body.tasks,
-                attendedEvents:req.body.attendedEvents,
-                experience:req.body.experience,
-                certificates:req.body.certificates
-            });
-            res.json({msg:'User was created successfully', data: newUser});
-        }else if(type=='partner'){
-            const isValidated = validator.createValidationPartner(req.body);
-            if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
-            const newUser =await User.create({
-                type:"partner",
-                name:req.body.name,
-                email:req.body.email,
-                password: passAfterHashing,
-                basicBussinesInformation:req.body.basicBussinesInformation,
-                boardMembers:req.body.boardMembers,
-                fieldOfWork:req.body.fieldOfWork,
-                partners:req.body.partners,
-                eventOrganized:req.body.eventOrganized,
-                formFeedBack:req.body.formFeedBack,
-                pastProjects:req.body.pastProjects,
-                contactInfo:req.body.contactInfo
-            });
-            res.json({msg:'User was created successfully', data: newUser});
-        }else{
-            const isValidated = validator.createValidationConsaltancyAgency(req.body);
-            if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
-            const newUser =await User.create({
-                type:"consultancy_agency",
-                name:req.body.name,
-                email:req.body.email,
-                password: passAfterHashing,
-                description:req.body.description,
-                specialization:req.body.specialization,
-                website:req.body.website,
-                fax:req.body.fax,
-                address:req.body.address,
-                reports:req.body.reports,
-                boardMembers:req.body.boardMembers,
-                partners:req.body.partners,
-                events:req.body.events
-            });
-            res.json({msg:'User was created successfully', data: newUser});
-        }
-   }
-   catch(error) {
-        res.json({error:error.message});
-   } 
-});
+router.post('/', users.postUser);
+router.put('/:id',users.putUser);
 
 
 // Update a user info 
@@ -213,25 +143,6 @@ router.post('/', async (req, res) => {
 //             res.json({error: error.message});
 //        } 
 // });
-        router.put('/:id', async (req, res) => {
-           
-                    const id = req.params.id;
-                    const user = await User.findOne({_id:id});
-                    if(!user) return res.status(404).send({error: 'User does not exist'});
-                    
-                    if (user.type=='member')
-                    {let result = validator.updateValidationMember(req.body) }
-                    if (user.type=='partner')
-                   {  result = validator.updateValidationPartner(req.body)} 
-                   else
-                   { result = validator.updateValidationConsaltancyAgency(req.body) }
-                    
-                    if (result.error) return res.status(400).send({ error: result.error.details[0].message })
-                    const updatedUser = await User.updateOne({ "_id" : id },req.body)
-                    res.json({data:updatedUser})
-        
-                   
-        });
 
 
 router.delete('/:id', async (req, res) => {
@@ -278,4 +189,4 @@ module.exports = router
 
 ///CHANGES
 //ghayart el response kollo gwa data ella elli bykkon b id
-//zawedt crud delete
+//zawedt crud deletegit 
