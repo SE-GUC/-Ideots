25dfaba34530b4cf0b0c0f2a5c504d0068bab500
Islam Kaloop <37816680+islamkaloop@users.noreply.github.com>
2019-04-05 23:32:51 +0200

    add controller to admin

---
+++ controllers/admins.js
@@ -0,0 +1,77 @@
+const bcrypt =require('bcryptjs');
+
+const Admin =require('../models/Admin');
+const validator =require('../validations/adminValidations');
+
+const Admins ={
+    getAdmins:async (req, res) =>{
+        const admins = await Admin.find()
+        res.json({ data: admins })
+    },
+
+    getAdmin:async (req, res) => {
+    try{
+        const adminId = req.params.id;
+        const admin =await Admin.findOne({"_id":adminId});
+        if(!admin) res.status(404).send({error:'Admin was not found'});
+        res.json(admin);
+    }catch(error) {
+        res.json({error:error.message});
+    } 
+    },
+
+    postAdmin: async (req, res) => {
+    try{
+        const isValidated = validator.createValidation(req.body);
+        if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
+        const salt =bcrypt.genSaltSync(10);
+        const passAfterHashing =bcrypt.hashSync(req.body.password,salt);
+        const newAdmin =await Admin.create({
+            name:req.body.name,
+            email:req.body.email,
+            password: passAfterHashing,
+            phone:req.body.phone
+        });
+        res.json({msg:'Admin was created successfully', data: newAdmin});
+   }
+   catch(error) {
+        res.json({error:error.message});
+   } 
+    },
+
+    putAdmin: async (req, res) => {
+    try {
+            const id = req.params.id;
+            const admin = await Admin.findOne({_id:id});
+            if(!admin) res.status(404).send({error:'Admin was not found'});
+            const isValidated = validator.updateValidation(req.body);
+            if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
+            const name =req.body.name;
+            const email =req.body.email;
+            const password =req.body.password;
+            const phone =req.body.phone;
+
+            if(name)await Admin.updateOne({_id:id},{$set:{name:name}});
+            if(email)await Admin.updateOne({_id:id},{$set:{email:email}});
+            if(password)await Admin.updateOne({_id:id},{$set:{password:password}});
+            if(phone)await Admin.updateOne({_id:id},{$set:{phone:phone}});
+            res.json({msg: 'Admin was updated successfully'});
+       }
+            catch(error) {
+            res.json({msg:error.message});
+       } 
+    },
+
+    deleteAdmin: async (req, res) => {
+    try{
+        const id = req.params.id;
+        const deletedAdmin = await Admin.deleteOne({"_id":id});
+        if(!deletedAdmin) res.status(404).send({error:'Admin was not found'});
+        res.json({msg:'Admin was deleted successfully', data: deletedAdmin});
+    }catch(error) {
+        res.json({error:error.message});
+   }
+    }
+}
+
+module.exports=Admins;
--- routes/api/admins.js
+++ routes/api/admins.js
@@ -1,82 +1,21 @@
 const express = require('express');
-const bcrypt =require('bcryptjs');
 const router = express.Router();
 
-const Admin =require('../../models/Admin');
-const validator =require('../../validations/adminValidations');
+const Admin =require('../../controllers/admins')
 
 // Get all admins
-router.get("/",async (req, res) =>{
-    const admins = await Admin.find()
-    res.json({ data: admins })
-});
+router.get("/",Admin.getAdmins);
 
 // Get a certain admin
-router.get('/:id', async (req, res) => {
-    try{
-        const adminId = req.params.id;
-        const admin =await Admin.findOne({"_id":adminId});
-        if(!admin) res.status(404).send({error:'Admin was not found'});
-        res.json(admin);
-    }catch(error) {
-        res.json({error:error.message});
-   } 
-});
+router.get('/:id', Admin.getAdmin);
 
 // Create a admin
-router.post('/', async (req, res) => {
-    try{
-        const isValidated = validator.createValidation(req.body);
-        if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
-        const salt =bcrypt.genSaltSync(10);
-        const passAfterHashing =bcrypt.hashSync(req.body.password,salt);
-        const newAdmin =await Admin.create({
-            name:req.body.name,
-            email:req.body.email,
-            password: passAfterHashing,
-            phone:req.body.phone
-        });
-        res.json({msg:'Admin was created successfully', data: newAdmin});
-   }
-   catch(error) {
-        res.json({error:error.message});
-   } 
-});
+router.post('/', Admin.postAdmin);
 
 // Update a admin info
-router.put('/:id', async (req, res) => {
-    try {
-            const id = req.params.id;
-            const admin = await Admin.findOne({_id:id});
-            if(!admin) res.status(404).send({error:'Admin was not found'});
-            const isValidated = validator.updateValidation(req.body);
-            if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message });
-            const name =req.body.name;
-            const email =req.body.email;
-            const password =req.body.password;
-            const phone =req.body.phone;
-
-            if(name)await Admin.updateOne({_id:id},{$set:{name:name}});
-            if(email)await Admin.updateOne({_id:id},{$set:{email:email}});
-            if(password)await Admin.updateOne({_id:id},{$set:{password:password}});
-            if(phone)await Admin.updateOne({_id:id},{$set:{phone:phone}});
-            res.json({msg: 'Admin was updated successfully'});
-       }
-            catch(error) {
-            res.json({msg:error.message});
-       } 
-});
+router.put('/:id', Admin.putAdmin);
 
 // Delete a admin
-router.delete('/:id', async (req, res) => {
-    try{
-        const id = req.params.id;
-        const deletedAdmin = await Admin.deleteOne({"_id":id});
-        if(!deletedAdmin) res.status(404).send({error:'Admin was not found'});
-        res.json({msg:'Admin was deleted successfully', data: deletedAdmin});
-    }catch(error) {
-        res.json({error:error.message});
-   }
-});
+router.delete('/:id',Admin.deleteAdmin);
 
 module.exports = router
