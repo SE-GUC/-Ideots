7f2729a5597b5353c5ceab46fff601276fd8125d
Islam Kaloop <37816680+islamkaloop@users.noreply.github.com>
2019-04-07 14:11:05 +0200

    update URL

--- functions/application_functions.js
+++ functions/application_functions.js
@@ -1,23 +1,23 @@
 const axios = require('axios');
 const functions = {
         postApplication: async (app) => {
-                const application=await axios.post('http://localhost:3000/api/application',app);
+                const application=await axios.post('https://lirten-hub-ideots.herokuapp.com/api/application',app);
                 return application;
         },
         getAllApplication: async () => {
-            const application = await axios.get('http://localhost:3000/api/application/');
+            const application = await axios.get('https://lirten-hub-ideots.herokuapp.com/api/application/');
             return application;
         },
         getApplication: async (id) => {
-            const application = await axios.get('http://localhost:3000/api/application/'+id);
+            const application = await axios.get('https://lirten-hub-ideots.herokuapp.com/api/application/'+id);
             return application;
         },
         updateApplication: async (id,app) => {
-            const application = await axios.put('http://localhost:3000/api/application/'+id,app);
+            const application = await axios.put('https://lirten-hub-ideots.herokuapp.com/api/application/'+id,app);
             return application;
         },
         deleteApplication: async (id) => {
-            const application = await axios.delete('http://localhost:3000/api/application/'+id);
+            const application = await axios.delete('https://lirten-hub-ideots.herokuapp.com/api/application/'+id);
             return application;
         }
 }; 
