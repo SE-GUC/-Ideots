39b774d4eeedcfea5d4082ef01b3dee0d894f29a
Islam Kaloop <37816680+islamkaloop@users.noreply.github.com>
2019-04-07 14:08:36 +0200

    add test errors correct

--- tests/application/test.js
+++ tests/application/test.js
@@ -1,4 +1,4 @@
-const funcs = require('./function');
+const funcs = require('../../functions/application_functions');
 
 const config ={
   id:null,
@@ -9,6 +9,7 @@ const config ={
 }
 
 test('Randomly creating a new application',async () => {
+    expect.assertions(3);
     const app = {
         applicantId: "5c9fe28433caec2078bfa349",
         taskId:"5c9556fc528e180fd91a0b1b"
@@ -28,6 +29,7 @@ test('Randomly creating a new application',async () => {
   });
 
   test('Fetching the data of that random application', async () => {
+    expect.assertions(6);
     const response =  await funcs.getApplication(config.id);
      // check if the json response has data not error
      expect(Object.keys(response.data)).toContain('data');
@@ -40,6 +42,7 @@ test('Randomly creating a new application',async () => {
   });
 
   test('Updating the data of that random application', async () => {
+    expect.assertions(5);
     const app = {
         date:"2017-04-30T23:34:28.802Z",
         acceptance:1
@@ -61,6 +64,7 @@ test('Randomly creating a new application',async () => {
   });
 
   test(`Deleting that random application and test getallapplications`, async () => {
+    expect.assertions(1);
     const lengthBefor =await funcs.getAllApplication();
     await funcs.deleteApplication(config.id);
     const lengthAfter =await funcs.getAllApplication();
@@ -69,46 +73,32 @@ test('Randomly creating a new application',async () => {
 
   test('Check if validaion return status 400 in post function', async()=>{
     try{
+        expect.assertions(1);
         const app = {
             taskId:"5c9556fc528e180fd91a0b1b"
         }
-        const response =  await funcs.postApplication(app);
-        // check if the json response has data not error
-        expect(Object.keys(response.data)).toContain('data');
-        expect(Object.keys(response.data)).not.toContain('error');
-
-        const application = await funcs.getApplication(response.data.data["_id"]);
-        expect(application.data.data).toMatchObject(response.data.data);
-        config.id = application.data.data._id;
-        config.applicantId = application.data.data.applicantId;
-        config.taskId = application.data.data.taskId;
-        config.date=application.data.data.date;
-        config.acceptance=application.data.data.acceptance;
+        await funcs.postApplication(app);
     }catch(error){
         expect(error.response.status).toBe(400)
     }
   });
   test('Check if validaion return status 400 in put function', async()=>{
     try{
+        expect.assertions(1);
         const app = {
             date:"2017-04-30T23:34:28.802Z",
             acceptance:"fff"
         }
-        const response =  await funcs.updateApplication(config.id,app);
-        // check if the json response has data not error
-        expect(Object.keys(response.data)).toContain('data');
-        expect(Object.keys(response.data)).not.toContain('error');
-    
-        const application = await funcs.getApplication(config.id);
-        expect(application.data.data).not.toEqual(null);
-        expect(application.data.data.date).toEqual("2017-04-30T23:34:28.802Z");
-        expect(application.data.data.acceptance).toBe(1);
-        config.id = application.data.data._id;
-        config.applicantId = application.data.data.applicantId;
-        config.taskId = application.data.data.taskId;
-        config.date=application.data.data.date;
-        config.acceptance=application.data.data.acceptance;
+        await funcs.updateApplication(config.id,app);
     }catch(error){
         expect(error.response.status).toBe(400)
         }
+   });
+   test('Check if getapplication return status 404 if id not found', async()=>{
+    try{
+        expect.assertions(1);
+        await funcs.getApplication(config.id);
+    }catch(error){
+        expect(error.response.status).toBe(404)
+    }
    });
