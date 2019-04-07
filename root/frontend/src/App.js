dc627f5bf1f3a0975a8bc454077f14b3e3d8a79b
mohanad <mohannad.mostafa98@gmail.com>
2019-04-07 17:56:55 +0300

    added Events component

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
+import Events from "./components/Events";
 
 class App extends Component {
   render() {
