06b924f57829e73d282347f1a9db4f8b91b6db63
Islam Kaloop <37816680+islamkaloop@users.noreply.github.com>
2019-04-11 04:04:57 +0200

    create class for eventcard

---
+++ root/frontend/src/components/event_components/EventCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class EventCard extends React.Component {
+  render() {
+    const { event } = this.props;
+
+    return <h3>hello world</h3>;
+  }
+}
+
+export default EventCard;
