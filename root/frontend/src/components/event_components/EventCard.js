2fc439c2938633539b6f422fc6b80da6c941ec03
Islam Kaloop <37816680+islamkaloop@users.noreply.github.com>
2019-04-11 04:07:13 +0200

    create html theam for eventcard

--- root/frontend/src/components/event_components/EventCard.js
+++ root/frontend/src/components/event_components/EventCard.js
@@ -1,10 +1,38 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
 
 class EventCard extends React.Component {
   render() {
     const { event } = this.props;
-
-    return <h3>hello world</h3>;
+    const remainSeat = event.numberOfSpaces - event.numberOfRegisterations;
+    const date = new Date(event.dateTime);
+    const day = date.toLocaleDateString("en-us", { weekday: "long" });
+    const monthName = date.toString().split(" ")[1];
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    return (
+      <Card>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe">{event.type.substring(0, 1)}</Avatar>
+          }
+          title={event.type + " in " + event.topics + " with " + event.speakers}
+          subheader={day + ", " + monthName + " " + month + ", " + year}
+        />
+        <CardContent>
+          <Typography component="p">{event.description}</Typography>
+        </CardContent>
+        <div>
+          <h1> </h1>
+          <div>{remainSeat + " Remaining Seat"}</div>
+          <div>{event.registrationPrice}</div>
+        </div>
+      </Card>
+    );
   }
 }
 
