b83c24f6bfae55d2aa518bc83571e4b0ae48e5ab
Islam Kaloop <37816680+islamkaloop@users.noreply.github.com>
2019-04-11 04:08:01 +0200

    adding style for card

--- root/frontend/src/components/event_components/EventCard.js
+++ root/frontend/src/components/event_components/EventCard.js
@@ -15,21 +15,59 @@ class EventCard extends React.Component {
     const month = date.getMonth();
     const year = date.getFullYear();
     return (
-      <Card>
+      <Card
+        style={{
+          marginBottom: "10px",
+          border: "1px solid blue"
+        }}
+      >
         <CardHeader
           avatar={
-            <Avatar aria-label="Recipe">{event.type.substring(0, 1)}</Avatar>
+            <Avatar aria-label="Recipe" style={{ background: "#20d86a" }}>
+              {event.type.substring(0, 1)}
+            </Avatar>
           }
+          style={{ background: "#58c6ff" }}
           title={event.type + " in " + event.topics + " with " + event.speakers}
           subheader={day + ", " + monthName + " " + month + ", " + year}
         />
         <CardContent>
           <Typography component="p">{event.description}</Typography>
         </CardContent>
-        <div>
+        <div style={{ display: "flex", position: "relative" }}>
           <h1> </h1>
-          <div>{remainSeat + " Remaining Seat"}</div>
-          <div>{event.registrationPrice}</div>
+          <div
+            style={{
+              position: "absolute",
+              width: "70%",
+              height: "60px",
+              backgroundColor: "#58c6ff",
+              opacity: ".8",
+              lineHeight: "40px",
+              textAlign: "center",
+              fontSize: "20px",
+              marginBottom: "5px"
+            }}
+          >
+            {remainSeat + " Remaining Seat"}
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              right: "0px",
+              width: "30%",
+              height: "60px",
+              backgroundColor: "#58c6ff",
+              opacity: ".8",
+              lineHeight: "40px",
+              textAlign: "center",
+              fontSize: "30px",
+              marginBottom: "5px",
+              border: "1px solid blue"
+            }}
+          >
+            {event.registrationPrice}
+          </div>
         </div>
       </Card>
     );
