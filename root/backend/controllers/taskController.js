335c16254284319a3b7d4a162a2d342040b22ace
AhmedSoror <ahmedsoror438@gmail.com>
2019-04-12 12:07:41 +0200

    task notifications still in progress

--- root/backend/controllers/taskController.js
+++ root/backend/controllers/taskController.js
@@ -1,8 +1,9 @@
 const Task = require("../models/Task");
+const notificationController=require('../controllers/notificationController');
 
-exports.getOneTask = async function(taskID) {    
+exports.getOneTask = async function(taskId) {    
     try{ 
-        const task = await Task.findOne({"_id":taskID})
+        const task = await Task.findOne({"_id":taskId})
         if(!task)
              return res.status(400).send({error: 'Task does not exist'})
         return task;
@@ -12,3 +13,20 @@ exports.getOneTask = async function(taskID) {
           console.log(error)   
         }
 };
+
+exports.notifyUser=async function (taskId,recieverId,content){
+  try{
+    const task = await Task.findOne({"_id":taskId})
+    recieverId=task.partnerID
+        body={           
+                "content": content + ' ,'+taskId ,
+                "recieverId": recieverId,
+                "notifierId": taskID
+            }
+        await notificationController.postNotification(body);
+
+  }
+  catch(error){
+    console.log(error)
+  }
+}
