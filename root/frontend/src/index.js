d4ea87d60a4b73048598ff431714891cae502f74
AhmedSoror <ahmedsoror438@gmail.com>
2019-04-13 02:16:16 +0200

    events notifications done pending real life tests

--- root/frontend/src/index.js
+++ root/frontend/src/index.js
@@ -6,7 +6,6 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
-import App from './App';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
