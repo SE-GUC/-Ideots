e272673affda05cc2962a8e097547ef21bfa0ba9
AhmedSoror <ahmedsoror438@gmail.com>
2019-04-14 19:23:49 +0200

    notification cards

--- root/frontend/src/components/notification_components/NotificationItem.js
+++ root/frontend/src/components/notification_components/NotificationItem.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
 
 class NotificationItem extends Component {
   readNotification = () => {};
@@ -11,35 +16,72 @@ class NotificationItem extends Component {
   };
   render() {
     const { content, date, _id, notifierId, isRead } = this.props.notifications;
+    const color=isRead?"#FF9100":"#4a26fd";
     return (
-      <div
-        style={this.notificationStyle()}
-        onClick={this.props.readNotification.bind(this, _id)}
-      >
-        <p style={{ flex: 1 }}>
-          {" "}
-          {"Content: "}
-          {content}
-        </p>
-        <p style={{ flex: 1 }}>
-          {" "}
-          {"Notifier_Id: "}
-          {notifierId}
-        </p>
-        <p style={{ flex: 1 }}>
-          {" "}
-          {"Date: "}
-          {date}
-        </p>
-        <p style={{ flex: 1 }}>
-          {" "}
-          {"Seen: "}
-          {isRead ? "Yes" : "No"}
-        </p>
-      </div>
+     
+
+     <Card style={{ border: "1px solid blue", margin: "2%",marginLeft:"25%",marginRight:"25%"}}  onClick={this.props.readNotification.bind(this, _id)} >
+     
+     <CardContent>
+       <Typography component="p">{`${content}`}</Typography>
+     </CardContent>
+     <div style={{ display: "flex", position: "relative" }}>
+       <h1> </h1>
+       <div
+         style={{
+           position: "absolute",
+           width: "60%",
+           height: "100px",
+           //backgroundColor: "#FF9100",//58c6ff,//4a26fd
+           backgroundColor:color,
+           opacity: ".8",
+           lineHeight: "40px",
+           textAlign: "center",
+           fontSize: "20px",
+           marginBottom: "5px",
+           marginLeft:"20%",
+           marginRight:"20%",
+         }}
+       >
+         {'L51'}
+       </div>
+    
+     </div>
+   </Card>
+
     );
   }
 }
+/*
+{
+<div
+  style={this.notificationStyle()}
+  onClick={this.props.readNotification.bind(this, _id)}
+>
+  <p style={{ flex: 1 }}>
+    {" "}
+    {"Content: "}
+    {content}
+  </p>
+  <p style={{ flex: 1 }}>
+    {" "}
+    {"Notifier_Id: "}
+    {notifierId}
+  </p>
+  <p style={{ flex: 1 }}>
+    {" "}
+    {"Date: "}
+    {date}
+  </p>
+  <p style={{ flex: 1 }}>
+    {" "}
+    {"Seen: "}
+    {isRead ? "Yes" : "No"}
+  </p>
+</div>
+
+}
+ */
 
 
 export default NotificationItem;
