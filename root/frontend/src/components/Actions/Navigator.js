915123ef0be911c59ec0f0c07f3f28364cb7717f
AhmedSoror <ahmedsoror438@gmail.com>
2019-04-21 03:44:36 +0200

    events link in sidebar

--- root/frontend/src/components/Actions/Navigator.js
+++ root/frontend/src/components/Actions/Navigator.js
@@ -33,7 +33,7 @@ const categories = [
       { id: 'Notifications', icon: <PublicIcon />, link: '/Main/Notifications' },
       { id: 'View Tasks', icon: <SettingsInputComponentIcon />, link: '/Main/Tasks'  },
       // { id: 'Submit final product', icon: <DnsRoundedIcon /> },
-      {id: 'View Events',icon: <PermMediaOutlinedIcon />,link: '/Main/Eventlist'},
+      {id: 'View Events',icon: <PermMediaOutlinedIcon />,link: '/Eventlist'},
       { id: 'My Profile', icon: <PeopleIcon />, link: '/Eventlist' }
     ]
   },
