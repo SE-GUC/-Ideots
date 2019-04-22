0c559b58887253cbfdc8e9a5a94de0a257a5fac7
MohammedHossam <mimo.mhf@gmail.com>
2019-04-22 21:44:56 +0200

    ind and pack

--- root/backend/index.js
+++ root/backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const passport = require("passport");
+var passport = require("passport");
 
 const config = require("./config/keys.js");
 
