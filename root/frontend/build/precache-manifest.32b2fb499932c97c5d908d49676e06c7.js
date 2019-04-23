5b3dab35275d663d12564e4cc390bfc5333d3978
AhmedSoror <ahmedsoror438@gmail.com>
2019-04-23 03:53:45 +0200

    deploy

---
+++ root/frontend/build/precache-manifest.32b2fb499932c97c5d908d49676e06c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8f3669aa89a555d728f2",
+    "url": "/static/js/main.8004d915.chunk.js"
+  },
+  {
+    "revision": "db09053e4dda73f2307b",
+    "url": "/static/js/2.58747caa.chunk.js"
+  },
+  {
+    "revision": "8f3669aa89a555d728f2",
+    "url": "/static/css/main.f7dc25c9.chunk.css"
+  },
+  {
+    "revision": "db09053e4dda73f2307b",
+    "url": "/static/css/2.7f5e7cdf.chunk.css"
+  },
+  {
+    "revision": "5daa4289ffa6fe9ae196150d8597b697",
+    "url": "/index.html"
+  }
+];
